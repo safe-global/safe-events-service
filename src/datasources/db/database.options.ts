69976155cf43afae92e98acd774ec1a7be0081ca
Moisés Fernández <7888669+moisses89@users.noreply.github.com>
2024-01-15 13:12:52 +0100

    Add configuration for SSL database connection (#140)

--- src/datasources/db/database.options.ts
+++ src/datasources/db/database.options.ts
@@ -1,6 +1,6 @@
 import { Webhook } from '../../routes/webhook/entities/webhook.entity';
 import { DataSourceOptions } from 'typeorm';
-
+import { readFileSync } from 'fs';
 /**
  * Use process.env for configuration instead of Nest.js ConfigService
  * as it cannot be used by TypeORM CLI to generate and run migrations
@@ -10,4 +10,16 @@ export const dataSourceOptions: DataSourceOptions = {
   url: process.env.MIGRATIONS_DATABASE_URL,
   entities: [Webhook],
   migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+  ...(process.env.DATABASE_SSL_ENABLED == 'true'
+    ? {
+        ssl: process.env.DATABASE_CA_PATH
+          ? {
+              ca: readFileSync(process.env.DATABASE_CA_PATH),
+              rejectUnauthorized: true,
+            }
+          : {
+              rejectUnauthorized: false,
+            },
+      }
+    : {}),
 };
