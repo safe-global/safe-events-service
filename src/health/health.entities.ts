20c367df5d475b74f849d9f0f5866cff7a9f0589
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-04-10 14:46:04 +0200

    Refactor health endpoint into readiness and liveness

---
+++ src/health/health.entities.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum HealthStatus {
+  OK = 'OK',
+  KO = 'KO',
+}
+
+export class Health {
+  @ApiProperty({ enum: Object.values(HealthStatus) })
+  status: HealthStatus;
+
+  constructor(healthStatus: HealthStatus) {
+    this.status = healthStatus;
+  }
+}
--- src/routes/about/about.controller.ts
+++ src/routes/about/about.controller.ts
@@ -3,13 +3,13 @@ import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { About } from './entities/about.entity';
 import { AboutService } from './about.service';
 
-@ApiTags('about')
 @Controller({ path: 'about' })
+@ApiTags('about')
 export class AboutController {
   constructor(private readonly aboutService: AboutService) {}
 
-  @ApiOkResponse({ type: About })
   @Get()
+  @ApiOkResponse({ type: About })
   getAbout(): About {
     return this.aboutService.getAbout();
   }
