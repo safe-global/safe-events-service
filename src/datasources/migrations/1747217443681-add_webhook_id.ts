93e338a793eed5f9028ebe0e8180e8d84fa9a44f
moisses89 <7888669+moisses89@users.noreply.github.com>
2025-05-14 13:25:16 +0200

    Set migrations

--- src/datasources/migrations/1747217443681-add_webhook_id.ts
+++ src/datasources/migrations/1747217443681-add_webhook_id.ts
@@ -4,6 +4,7 @@ export class AddWebhookId1747217443681 implements MigrationInterface {
   name = 'AddWebhookId1747217443681';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // Update id
     await queryRunner.query(
       `ALTER TABLE "webhook" DROP CONSTRAINT "PK_e6765510c2d078db49632b59020"`,
     );
@@ -14,30 +15,29 @@ export class AddWebhookId1747217443681 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "webhook" ADD CONSTRAINT "PK_e6765510c2d078db49632b59020" PRIMARY KEY ("id")`,
     );
-    await queryRunner.query(`ALTER TABLE "webhook" DROP COLUMN "url"`);
+    // Update url column
     await queryRunner.query(
-      `ALTER TABLE "webhook" ADD "url" character varying(300) NOT NULL`,
+      'ALTER TABLE "webhook" ALTER COLUMN "url" TYPE character varying(300);',
     );
     await queryRunner.query(
       `ALTER TABLE "webhook" ADD CONSTRAINT "UQ_1d66ccfc5321ea9310100a77cf6" UNIQUE ("url")`,
     );
-    await queryRunner.query(`ALTER TABLE "webhook" DROP COLUMN "description"`);
+    // Update description column
     await queryRunner.query(
-      `ALTER TABLE "webhook" ADD "description" character varying(300) NOT NULL`,
+      `ALTER TABLE "webhook" ALTER COLUMN "description" TYPE character varying(300);`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "webhook" DROP COLUMN "description"`);
+    // Rollback migration for description
     await queryRunner.query(
-      `ALTER TABLE "webhook" ADD "description" character varying NOT NULL`,
+      `ALTER TABLE "webhook" ALTER COLUMN "description" TYPE character varying`,
     );
     await queryRunner.query(
       `ALTER TABLE "webhook" DROP CONSTRAINT "UQ_1d66ccfc5321ea9310100a77cf6"`,
     );
-    await queryRunner.query(`ALTER TABLE "webhook" DROP COLUMN "url"`);
     await queryRunner.query(
-      `ALTER TABLE "webhook" ADD "url" character varying NOT NULL`,
+      `ALTER TABLE "webhook" ALTER COLUMN "url" TYPE character varying`,
     );
     await queryRunner.query(
       `ALTER TABLE "webhook" DROP CONSTRAINT "PK_e6765510c2d078db49632b59020"`,
