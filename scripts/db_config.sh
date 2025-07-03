3e83fb92b43bea85d88a412618bb563ae7b79eae
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-07-03 13:18:45 +0200

    Fix migrations path in db config script (#424)

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -2,5 +2,5 @@
 
 export MIGRATIONS_DATABASE_URL=${MIGRATIONS_DATABASE_URL:-psql://postgres:postgres@localhost:5433/migrations}
 ORMCONFIG="./src/datasources/db/ormconfig.ts"
-MIGRATIONS_FOLDER="./src/datasources/migrations"
+MIGRATIONS_FOLDER="./src/datasources/db/migrations"
 MIGRATIONS_NAME=${1:-MIGRATION_NAME_NOT_DEFINED}
